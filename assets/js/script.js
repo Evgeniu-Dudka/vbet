5bf66af126e27952f871e24c273ce0df157015e2
Evgeniu Dudka <131858472+Evgeniu-Dudka@users.noreply.github.com>
2024-03-11 16:52:01 +0300

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+"use strict"
+
+const iconMenu = document.querySelector('.menu__icon');
+if (iconMenu) {
+	const headerMenu = document.querySelector('.header__menu');
+	iconMenu.addEventListener("click", function (e) {
+		document.body.classList.toggle('_lock');
+		iconMenu.classList.toggle('_active');
+		headerMenu.classList.toggle('_active');
+	}) 
+}
